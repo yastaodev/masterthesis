ff76c67dc31d324a01fadc26d6576f5e83e89824
yast <me@yast.com>
2022-06-10 10:10:17 +0200

    Update

--- usecase-03/src/main/java/com/yast/masterthesis/usecase03/Main.java
+++ usecase-03/src/main/java/com/yast/masterthesis/usecase03/Main.java
@@ -1,39 +1,73 @@
 package com.yast.masterthesis.usecase03;
 
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
     public static String PYTHON = "python";
-    private static String SOURCE_FILE_NAME = "step01.py";
-    private static InputStream SOURCE_FILE_INPUT = Main.class.getClassLoader().getResourceAsStream(SOURCE_FILE_NAME);
+    private static String SOURCE_FILE_NAME = "/opt/work/workspaces/idea/master-thesis/usecase-03/src/main/resources/step01.py";
+    //private static InputStream SOURCE_FILE_INPUT = Main.class.getClassLoader().getResourceAsStream(SOURCE_FILE_NAME);
 
     public static void main(String[] args) {
         Path path = Paths.get("venv", "bin", "graalpython");
-        String VENV_EXECUTABLE = path.toString();
+        String VENV_EXECUTABLE = "/opt/work/workspaces/idea/master-thesis/usecase-03/venv/bin/graalpython";//path.toAbsolutePath().toString();
         try (Context context = Context.newBuilder("python").
+                allowNativeAccess(true).
                 allowAllAccess(true).
                 option("python.ForceImportSite", "true").
                 option("python.Executable", VENV_EXECUTABLE).
-                build();) {
+                build();
+        ) {
+            context.eval(PYTHON, "import site");
             context.eval(PYTHON, "print('Hello Python!')");
 
             context.eval(PYTHON, "import sys; print(sys.version)");
+            context.eval(PYTHON, "import sys; print(sys.executable)");
 
-            InputStreamReader reader = new InputStreamReader(SOURCE_FILE_INPUT);
+
+
+            //InputStreamReader reader = new InputStreamReader(SOURCE_FILE_INPUT);
+            System.out.println("#####" + SOURCE_FILE_NAME);
             Source source;
             try {
-                source = Source.newBuilder(PYTHON, reader, SOURCE_FILE_NAME).build();
+                source = Source.newBuilder(PYTHON, new File(SOURCE_FILE_NAME)).build();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            System.out.println(source.toString());
             context.eval(source);
+        }
+
+        try {
+
+            // Use this to Read a Local Path
+            // String svgUriImputLocation = Paths.get("https://www.contradodigital.com/logo.svg").toUri().toURL().toString();
+            // Read Remote Location for SVG
+            String svgUriImputLocation = "/opt/work/workspaces/idea/master-thesis/usecase-03/target/bar_code.svg";
+            TranscoderInput transcoderInput = new TranscoderInput(svgUriImputLocation);
+
+            // Define OutputStream Location
+            OutputStream outputStream = new FileOutputStream("/home/yastao/mymy.png");
+            TranscoderOutput transcoderOutput = new TranscoderOutput(outputStream);
+
+            // Convert SVG to PNG and Save to File System
+            PNGTranscoder pngTranscoder = new PNGTranscoder();
+            pngTranscoder.transcode(transcoderInput, transcoderOutput);
+
+            // Clean Up
+            outputStream.flush();
+            outputStream.close();
+
+        } catch (IOException | TranscoderException ex) {
+            System.out.println("Exception Thrown: " + ex);
         }
     }
 }
--- usecase-03/src/main/resources/step01.py
+++ usecase-03/src/main/resources/step01.py
@@ -1,8 +1,45 @@
 #improting the module
+import site
 import qrcode
+from PIL import Image
 
-#creating a variable for the text in which we want the QR code to link us
-qr = qrcode.make('Hello Programmers')
+qr = qrcode.QRCode(
+    version=12,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=4,
+    border=8
+)
+qr.add_data('test text')
+qr.make()
+img = qr.make_image(fill_color="red", back_color="yellow")
+img.save('/opt/work/workspaces/idea/master-thesis/usecase-03/target/qrcode.png')
 
-#saving the QR code as a png file
-qr.save('../../../target/qrcode.png')
+# -----------------------------------------------------------------------
+img_bg = Image.open('/opt/work/workspaces/idea/master-thesis/usecase-03/target/qrcode.png')
+
+qr = qrcode.QRCode(box_size=2)
+qr.add_data('I am Lena')
+qr.make()
+img_qr = qr.make_image()
+
+pos = (img_bg.size[0] - img_qr.size[0], img_bg.size[1] - img_qr.size[1])
+
+img_bg.paste(img_qr, pos)
+img_bg.save('/opt/work/workspaces/idea/master-thesis/usecase-03/target/qr_lena.png')
+
+# -----------------------------------------------------------------------
+
+
+
+# importing EAN13 from the python-barcode module
+from barcode import EAN13
+
+# add any 12 digit number you would like to
+number = '5909876123457'
+
+# Now, let's create an object of EAN13
+# make a class and pass the variable number created above
+my_code = EAN13(number)
+
+# save it
+my_code.save('/opt/work/workspaces/idea/master-thesis/usecase-03/target/bar_code')
